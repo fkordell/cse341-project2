a66111469b6346c1e49ce07025f3f9ce4392116d
fkordell <far18009@byui.edu>
2024-02-16 17:22:34 -0700

    updates

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 require('dotenv').config(); 
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(express.json());
-app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+})
 app.use('/', require('./routes'));
 app.use((err, req, res, next) => {
   console.error(err.stack);
