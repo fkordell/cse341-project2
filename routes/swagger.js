ff378a41b33dcb82627a5a4f3feda9fb1fcbf0a4
fkordell <far18009@byui.edu>
2024-03-01 18:28:41 -0700

    updates

--- routes/swagger.js
+++ routes/swagger.js
@@ -4,4 +4,17 @@ const swaggerDocument = require('../swagger.json');
 router.use('/api-docs', swaggerUi.serve);
 router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
+const swaggerUiOptions = {
+    swaggerOptions: {
+      oauth2RedirectUrl: 'https://cse341-project2-h8z8.onrender.com/api-docs/oauth2-redirect.html',
+      oauth: {
+        clientId: 'uLkMoDSftuzSO9hW76bLyVdT5CrRNgdh',
+        usePkceWithAuthorizationCodeGrant: true, 
+      }
+    }
+  };
+  
+  router.use('/api-docs', swaggerUi.serve);
+  router.get('/api-docs', swaggerUi.setup(swaggerDocument, swaggerUiOptions));
+
 module.exports = router;
